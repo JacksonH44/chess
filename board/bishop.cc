4398330d58dc8cba3c96a768f05070cb12c80896
Jackson Howe <jacksonhowe3@gmail.com>
2022-11-26 22:37:24 -0500

    bishop validity done

--- board/bishop.cc
+++ board/bishop.cc
@@ -1,4 +1,7 @@
 #include "bishop.h"
+#include "board.h"
+#include <vector>
+#include <algorithm>
 
 Bishop::Bishop(int colour, pos position) : Piece{colour, &position}, type{'b'} {
     if (colour == 1)
@@ -21,8 +24,116 @@ char Bishop::getType() const {
 void Bishop::updateValidMoves(Board* board, pos p) {
     this->position->x = p.x;
     this->position->y = p.y;
+    bool upRight = true, upLeft = true, downRight = true, downLeft = true;
+    int i = 1;
+    while (upRight || upLeft || downRight || downLeft) {
+        if (upRight)
+        {
+            pos tmpPos = pos{this->position->x + i, this->position->y + i};
+            if (!tmpPos.inBounds())
+            { // position out of bounds
+                upRight = false;
+            }
+            else
+            {
+                Piece *tmpPiece = board->getPiece(tmpPos);
+                if (tmpPiece != nullptr)
+                {
+                    if (tmpPiece->getColour() != this->colour)
+                    { // killing move
+                        validMoves.emplace_back(tmpPos);
+                    }
+                    upRight = false;
+                }
+                else
+                {
+                    validMoves.emplace_back(tmpPos);
+                }
+            }
+        }
+        if (upLeft)
+        {
+            pos tmpPos = pos{this->position->x - i, this->position->y + i};
+            if (!tmpPos.inBounds())
+            { // position out of bounds
+                upLeft = false;
+            }
+            else
+            {
+                Piece *tmpPiece = board->getPiece(tmpPos);
+                if (tmpPiece != nullptr)
+                {
+                    if (tmpPiece->getColour() != this->colour)
+                    { // killing move
+                        validMoves.emplace_back(tmpPos);
+                    }
+                    upLeft = false;
+                }
+                else
+                {
+                    validMoves.emplace_back(tmpPos);
+                }
+            }
+        }
+        if (downRight)
+        {
+            pos tmpPos = pos{this->position->x + i, this->position->y - i};
+            if (!tmpPos.inBounds())
+            { // position out of bounds
+                downRight = false;
+            }
+            else
+            {
+                Piece *tmpPiece = board->getPiece(tmpPos);
+                if (tmpPiece != nullptr)
+                {
+                    if (tmpPiece->getColour() != this->colour)
+                    { // killing move
+                        validMoves.emplace_back(tmpPos);
+                    }
+                    downRight = false;
+                }
+                else
+                {
+                    validMoves.emplace_back(tmpPos);
+                }
+            }
+        }
+        if (downLeft)
+        {
+            pos tmpPos = pos{this->position->x - i, this->position->y - i};
+            if (!tmpPos.inBounds())
+            { // position out of bounds
+                downLeft = false;
+            }
+            else
+            {
+                Piece *tmpPiece = board->getPiece(tmpPos);
+                if (tmpPiece != nullptr)
+                {
+                    if (tmpPiece->getColour() != this->colour)
+                    { // killing move
+                        validMoves.emplace_back(tmpPos);
+                    }
+                    downLeft = false;
+                }
+                else
+                {
+                    validMoves.emplace_back(tmpPos);
+                }
+            }
+        }
+        ++i;
+    }
 }
 
 bool Bishop::validate(pos p, Board* board) {
-    return false;
+    if (find(validMoves.begin(), validMoves.end(), p) != validMoves.end())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
