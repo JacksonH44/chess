f1b92e11fd0de07c41646a52785bb23a23e5272a
Jackson Howe <jacksonhowe3@gmail.com>
2022-11-27 14:09:16 -0500

    clean makefile

--- board/king.d
+++
@@ -1,2 +0,0 @@
-board/king.o: board/king.cc board/king.h board/piece.h board/../pos.h \
- board/board.h
--- player/cpu1.d
+++
@@ -1,2 +0,0 @@
-player/cpu1.o: player/cpu1.cc player/cpu1.h player/player.h \
- player/../pos.h
