02d27b6b6fc1728b8058b893b10ecaf54bf4c489
Jackson Howe <jacksonhowe3@gmail.com>
2022-11-26 22:31:38 -0500

    rook and king sans castling

--- board/king.cc
+++ board/king.cc
@@ -1,4 +1,7 @@
 #include "king.h"
+#include "board.h"
+#include <vector>
+#include <algorithm>
 
 King::King(int colour, pos position, bool castle) : Piece{colour, &position}, canCastle{canCastle}, type{'k'} {
     if (colour == 1)
@@ -26,8 +29,93 @@ char King::getType() const
 void King::updateValidMoves(Board* board, pos p) {
     this->position->x = p.x;
     this->position->y = p.y;
+
+    // move right
+    pos tmpPos = pos{this->position->x + 1, this->position->y};
+    if (tmpPos.inBounds()) {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour) {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
+
+    // move left
+    tmpPos = pos{this->position->x - 1, this->position->y};
+    if (tmpPos.inBounds())
+    {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour)
+        {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
+
+    // move down
+    tmpPos = pos{this->position->x, this->position->y + 1};
+    if (tmpPos.inBounds())
+    {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour)
+        {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
+
+    // move up
+    tmpPos = pos{this->position->x, this->position->y - 1};
+    if (tmpPos.inBounds())
+    {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour)
+        {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
+
+    // move right down
+    tmpPos = pos{this->position->x + 1, this->position->y + 1};
+    if (tmpPos.inBounds())
+    {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour)
+        {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
+
+    // move right up
+    tmpPos = pos{this->position->x + 1, this->position->y - 1};
+    if (tmpPos.inBounds())
+    {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour)
+        {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
+
+    // move left down
+    tmpPos = pos{this->position->x - 1, this->position->y + 1};
+    if (tmpPos.inBounds())
+    {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour)
+        {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
+
+    // move left up
+    tmpPos = pos{this->position->x - 1, this->position->y - 1};
+    if (tmpPos.inBounds())
+    {
+        if (board->getPiece(tmpPos) == nullptr || board->getPiece(tmpPos)->getColour() != colour)
+        {
+            validMoves.emplace_back(tmpPos);
+        }
+    }
 }
 
 bool King::validate(pos p, Board* board) {
-    return false;
+    if (find(validMoves.begin(), validMoves.end(), p) != validMoves.end())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
