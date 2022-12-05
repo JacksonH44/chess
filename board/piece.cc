c87a2cad9cbe1043bb2e7070b69142b6e0c4e80b
Jackson Howe <jacksonhowe3@gmail.com>
2022-12-05 00:59:08 -0500

    add docs, clean up tests

--- board/piece.cc
+++ board/piece.cc
@@ -12,31 +12,31 @@ int Piece::getColour(){
 }
 
 
-bool Piece::isValidMove(pos p, Board* board){
+// bool Piece::isValidMove(pos p, Board* board){
 
-    // Check for valid position
-    if (!(((0 <= p.x) && (p.x <= 7)) && ((0 <= p.y) && (p.y <= 7)))) // Out of board dimensions
-    {
-        return false;
-    }
-    else if ((position.x == p.x) && (position.y == p.y)) // Not moving anywhere
-    {
-        return false;
-    }
+//     // Check for valid position
+//     if (!(((0 <= p.x) && (p.x <= 7)) && ((0 <= p.y) && (p.y <= 7)))) // Out of board dimensions
+//     {
+//         return false;
+//     }
+//     else if ((position.x == p.x) && (position.y == p.y)) // Not moving anywhere
+//     {
+//         return false;
+//     }
 
-    // Check for player killing their own piece
-    Piece *curPiece = board->getPiece(p);
-    if (curPiece != nullptr)
-    {
-        if (colour == curPiece->getColour())
-        {
-            return false;
-        }
-    }
+//     // Check for player killing their own piece
+//     Piece *curPiece = board->getPiece(p);
+//     if (curPiece != nullptr)
+//     {
+//         if (colour == curPiece->getColour())
+//         {
+//             return false;
+//         }
+//     }
 
-    // Individual piece validating
-    return validate(p, board);
-}
+//     // Individual piece validating
+//     return validate(p, board);
+// }
 
 vector<pos> Piece::getValidMoves() {
     return validMoves;
